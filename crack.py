8d43abd89ffe0a51d73b47c5d8774aa876896a7b
zaimeee <151784577+zaimeee@users.noreply.github.com>
2024-01-16 23:16:26 +0000

    Create crack.py

---
+++ crack.py
@@ -0,0 +1,14 @@
+from string import digits
+
+for i in digits:
+    for j in digits:
+        for k in digits:
+            for l in digits:
+                for m in digits:
+                    for n in digits:
+                        for o in digits:
+                            for p in digits:
+                                for q in digits:
+                                    for r in digits:
+                                        print(i, j, k, l, m, n, o, p, q, r)  
+                                        
